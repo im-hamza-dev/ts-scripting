a7f920332fb2622fc2af0aa1c1ea13b84a0980cd
Hamza Iqbal <hamza@shapeazuremysha.onmicrosoft.com>
2023-07-14 22:03:28 +0500

    added generics sample

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-console.log("hello world");
-let characterA = {
-  name: "Paul",
-  age: 21,
+interface User {
+  name: string;
+}
+const addId = <T>(obj: T) => {
+  let id = Math.floor(Math.random() * 100);
+  return { ...obj, id };
+};
+let userA = {
+  name: "Hamza",
+};
+userA = addId<User>(userA);
+
+console.log(userA);
+5255723053012510101;
+37401 - 6108918 - 5;
+interface Character<T> {
+  name: string;
+  age: number;
+  powers: T; // to make this dynamic type we can use GENERICS <Type>
+}
+let characterA: Character<object> = {
+  name: "Hamza",
+  age: 24,
+  powers: { lives: 2, canSwim: true },
 };
 
-console.log(characterA);
+let characterB: Character<string[]> = {
+  name: "Paul",
+  age: 20,
+  powers: ["Multiple Livers", "Can Swim"],
+};
+
+let characterC: Character<number[]> = {
+  name: "Paul",
+  age: 20,
+  powers: [4, 1],
+};
+
+console.log(characterA, characterB, characterC);
